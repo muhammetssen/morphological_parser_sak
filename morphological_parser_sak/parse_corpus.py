ad14029967c780477b671dfd40b36fb7b49d6f77
selim-karaduman <selim.karaduman07@gmail.com>
2020-11-28 20:18:10 +0300

    Morphological parser Cextensions port to python3.7

--- morphological_parser_sak/parse_corpus.py
+++ morphological_parser_sak/parse_corpus.py
@@ -7,30 +7,33 @@ import sys
 import re
 import TurkishMorphology
 
-if len(sys.argv) < 2:
-	print 'usage:', sys.argv[0], 'corpus[ex:test.txt]'
-	exit(1)
+def init():
+	TurkishMorphology.load_lexicon('turkish.fst')
 
-def parse_corpus():
-	TurkishMorphology.load_lexicon('turkish.fst');
-	n = 0
-	e = 0
-	f = open(sys.argv[1], 'r')
-	for line in f:
-		print '<S> <S>+BSTag'
+"""
+Returns list of lists: where each list is morphological parses of a sentence
+"""
+def parse_lines(text):
+	parsed_text = ""
+	lines = [x for x in text.strip().split("\n") if x]
+	for line in lines:
+		parsed_text += "<S> <S>+BSTag\n" 
 		line = line.rstrip()
 		words = re.split('\s+', line)
 		for w in words:
 			parses = TurkishMorphology.parse(w)
 			if not parses:
-				print w, w+"[Unknown]"
+				parsed_text += w + " " + w + "[Unknown]\n"
 				continue
-			print w,
+			parsed_text += w
 			for p in parses: #There may be more than one possible morphological analyses for a word
 				(parse, neglogprob) = p #An estimated negative log probability for a morphological analysis is also returned
-				print parse,
-			print
-		print '</S> </S>+ESTag'
-	f.close()
+				parsed_text += " " + parse
+			parsed_text += "\n"
+		parsed_text += "</S> </S>+ESTag\n"
+	return parsed_text
 
-parse_corpus()
+# Test
+if __name__ == '__main__':
+	init()
+	print(parse_lines("şekerlerinin"), end="")
