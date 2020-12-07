2b48b0885dba00f558a545ee44b9f7706494061f
selim-karaduman <selim.karaduman07@gmail.com>
2020-12-07 20:00:36 +0300

    fix package name and data files

--- setup.py
+++ setup.py
@@ -9,67 +9,36 @@ import subprocess
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open(os.path.join("morphological_parser_sak", "version.txt"), "r") as f:
+with open(os.path.join("morphological_parser", "version.txt"), "r") as f:
     __version__ = f.read().strip()
-
-with open(os.path.join("morphological_parser_sak", 
-                        "python_requirements.txt"), "r") as f:
-    requirements_list = []
-    #requirements_list = [d for d in f.read().split("\n") if d]
-
-
-class SetupDeps(install):
-    
-    def run(self):
-        print("?"*15)
-        if find_executable("perl") is None:
-            raise RuntimeError("Perl must be installed on the system")
-        pdep_folder = os.path.join("morphological_parser_sak", 
-                                        "perl_dependencies")
-        wd = os.getcwd()
-        os.chdir(pdep_folder)
-        output = subprocess.check_output(["bash", "INSTALL.sh"])
-        os.chdir(wd)
-        install.run(self)
 
 
 setuptools.setup(
-    name="morphological_parser_sak",
+    name="morphological_parser",
     version=__version__,
     author="Haşim Sak",
     author_email="tabilab.dip@gmail.com",
     description="Morphological Parser and Disambiguator with Python Bindings",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tabilab-dip/morphological_parser_sak",
+    url="https://github.com/tabilab-dip/morphological_parser",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: Linux",
     ],
-    cmdclass={
-        "install": SetupDeps
-    },
     package_data={
-        "morphological_parser_sak": [
-            "perl_dependencies/*",
-            "perl_dependencies/*/*",
-            "perl_dependencies/*/*/*",
-            "perl_dependencies/*/*/*/*",
-            "perl_dependencies/*/*/*/*/*",
-            "perl_dependencies/*/*/*/*/*/*",
-            "perl_dependencies/*/*/*/*/*/*/*",
-            "perl_dependencies/*/*/*/*/*/*/*/*",
-            "md.pl",
+        "morphological_parser": [
             "model.txt",
             "turkish.fst",
+            "TurkishMorphology.so"
         ]
     },
     exclude_package_data={ # we don't need api for packaged application
-        "morphological_parser_sak": [
+        "morphological_parser": [
             "api.py",
         ]
     },
-    install_requires=requirements_list,
+    install_requires=[], # no requirements, flask is needed for server
     python_requires='>=3.6',
 )
